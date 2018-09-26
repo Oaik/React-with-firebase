b179e0ba087099b727d1aaa233e4efb262252fc7
Omar Ashraf <omarashrf15@gmail.com>
2018-09-26 16:38:09 +0200

    Added redux

--- firebasereact/src/components/projects/ProjectList.js
+++ firebasereact/src/components/projects/ProjectList.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import ProjectSummary from './ProjectSummary'
 
-class ProjectList extends Component {
-    render() {
-        return (
-            <div className="project-list section">
-                <ProjectSummary />
-                <ProjectSummary />
-                <ProjectSummary />
-                <ProjectSummary />
-                <ProjectSummary />
-            </div>
-        );
-    }
+const ProjectList = ({projects}) => {
+    return (
+        <div className="project-list section">
+            { projects && projects.map(project => {
+                return(
+                    <ProjectSummary project={project} key={project.id}/>
+                )
+            }) 
+            }
+        </div>
+    );
 }
 
 export default ProjectList;
