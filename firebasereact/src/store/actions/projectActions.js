a2c0d2f1a3e60459df9ed8157f9547ba2c71d1c2
Omar Ashraf <omarashrf15@gmail.com>
2018-09-29 23:40:53 +0200

    Added Thunk redux

---
+++ firebasereact/src/store/actions/projectActions.js
@@ -0,0 +1,10 @@
+export const createProject = (project) => {
+    return (dispatch, getState) => {
+        // async code
+        dispatch({
+            type: "CREATE_PROJECT",
+            project
+        })
+    }
+        
+}
